b306cdc1da6029ec097c0de68ae2cd9fdc989275
Olutunmise Adeniji <tadeniji06@gmail.com>
2025-12-11 17:38:35 +0100

    op

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
+	// eslint: {
+	// 	// Warning: This allows production builds to successfully complete even if
+	// 	// your project has ESLint errors.
+	// 	ignoreDuringBuilds: true,
+	// },
 	typescript: {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has TypeScript errors.
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
 			{ hostname: "cdn.sanity.io" },
 			{ hostname: "imperiasseo.vercel.app" },
 		],
+		unoptimized: true,
 	},
 };
 
