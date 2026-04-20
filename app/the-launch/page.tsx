62680fd5ca9e193c9ab43e85808194cb405150eb
Olutunmise Adeniji <tadeniji06@gmail.com>
2026-04-20 08:15:44 +0100

    -ppp-

--- app/the-launch/page.tsx
+++ app/the-launch/page.tsx
@@ -131,7 +131,7 @@ export default function LaunchPage() {
 											</div>
 											<div className='mt-4'>
 												<h3 className='text-lg font-semibold text-gray-900'>{vid.title}</h3>
-												<p className='text-sm text-gray-500'>Video {vid.id}</p>
+												{/* <p className='text-sm text-gray-500'>Video {vid.id}</p> */}
 											</div>
 										</motion.div>
 									))}
